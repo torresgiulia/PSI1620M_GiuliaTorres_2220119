1c5c883a7643385d86fbbee25e394985824e02cb
PedroJuanTorres <74351644+PedroJuanTorres@users.noreply.github.com>
2022-05-15 16:24:46 +0100

    Conexão com a combobox

--- src/PSI1620M_GiuliaTorres_2220119_PROJETO/PSI1620M_GiuliaTorres_2220119_PROJETO/SignIn.cs
+++ src/PSI1620M_GiuliaTorres_2220119_PROJETO/PSI1620M_GiuliaTorres_2220119_PROJETO/SignIn.cs
@@ -17,36 +17,109 @@ namespace PSI1620M_GiuliaTorres_2220119_PROJETO
     {
         string connstring = ConfigurationManager.ConnectionStrings["cnGifty"].ConnectionString;
 
-        List<Concelhos> concelhos;
+        List<Concelhos> listConcelhos = new List<Concelhos>();
 
-        BindingList<Concelhos> BListConcelhos;
+        BindingList<Concelhos> bListConcelhos = new BindingList<Concelhos>();
 
         public SignIn()
         {
             InitializeComponent();
+            SqlConnection connection = new SqlConnection(connstring);
+
+
+            // Ligação à base de dados para ligar com a comboBox Concelho (CBconcelho)
+            try
+            {
+                connection.Open();
+                SqlCommand conc = connection.CreateCommand();
+                conc.CommandText = "select * from Concelhos";
+                
+
+                var ler = conc.ExecuteReader();
+
+                if (ler.HasRows)
+                {
+                    while (ler.Read())
+                    {
+                        Concelhos concelho = new Concelhos()
+                        {
+                            ConcelhoId = Convert.ToInt32(ler["id"].ToString()),
+                            ConcelhoNome = ler["nome"].ToString()
+                        };
+                        listConcelhos.Add(concelho);
+                    }
+                }
+                bListConcelhos = new BindingList<Concelhos>(listConcelhos.OrderBy(x => x.ConcelhoNome).ToList());
+
+                CBconcelho.DataSource = bListConcelhos;
+                CBconcelho.DisplayMember = "ConcelhoNome";
+                CBconcelho.ValueMember = "ConcelhoId";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
         }
 
         private void SignIn_Load(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(connstring);
-
-            // Ligação à base de dados para ligar com a comboBox Concelho (CBconcelho)
-            context.People.Load();
-            this.CBconcelho.DataSource = BListConcelhos = context.People.Local.ToBindingList();
-            this.CBconcelho.DisplayMember = "nome";
+            //SqlConnection connection = new SqlConnection(connstring);
 
 
+            //// Ligação à base de dados para ligar com a comboBox Concelho (CBconcelho)
+            //try
+            //{
+            //    connection.Open();
+            //    SqlCommand conc = connection.CreateCommand();
+            //    conc.CommandText = "select * from Concelhos";
+            //    connection.Open();
+
+            //    var ler = conc.ExecuteReader();
+
+            //    if (ler.HasRows)
+            //    {
+            //        while (ler.Read())
+            //        {
+            //            Concelhos concelho = new Concelhos()
+            //            {
+            //                ConcelhoId = Convert.ToInt32(ler["id"].ToString()),
+            //                ConcelhoNome = ler["nome"].ToString()
+            //            };
+            //            listConcelhos.Add(concelho);
+            //        }
+            //    }
+            //    bListConcelhos = new BindingList<Concelhos>(listConcelhos.OrderBy(x => x.ConcelhoNome).ToList());
+
+            //    CBconcelho.DataSource = bListConcelhos;
+            //    CBconcelho.DisplayMember = "ConcelhoNome";
+            //    CBconcelho.ValueMember = "ConcelhoId";
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+            //finally
+            //{
+            //    if (connection.State == ConnectionState.Open)
+            //    {
+            //        connection.Close();
+            //    }
+            //}            
 
 
+            //foreach (var conce in listConcelhos)
+            //{
+                
+            //}
 
-            concelhos = new List<Concelhos>();
-            BListConcelhos = new BindingList<Concelhos>()
-            {
-                from Concelhos
-                select *
-            };
-            CBconcelho.DataSource = BListConcelhos;
-            
 
 
 
@@ -59,34 +132,4 @@ namespace PSI1620M_GiuliaTorres_2220119_PROJETO
 
     }
 }
-/*try
-            {
-                connection.Open();
-                SqlCommand conc = connection.CreateCommand();
-                conc.CommandText = "select * from Concelhos";
-                connection.Open();
-
-                var ler = conc.ExecuteReader();
-                if(ler.HasRows)
-                {
-                    while(ler.Read())
-                    {
-                        Concelhos concelho = new Concelhos()
-                        {
-                            ConcelhoId = Convert.ToInt32(ler["id"].ToString()),
-                            ConcelhoNome = ler["nome"].ToString()
-                        };
-                    }
-                }
-            }
-            catch(Exception)
-            {
-                throw;
-            }
-            finally
-            {
-                if (connection.State == ConnectionState.Open)
-                {
-                    connection.Close();
-                }
-            }*/
+            
